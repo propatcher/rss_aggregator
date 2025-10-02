da120c49dbaf82a29b40b210c097b2489b3a5c11
propatcher <dfyz240206@gmail.com>
2025-10-02 13:19:20 +0400

    full models + alembic revisions

--- app/Article/models.py
+++ app/Article/models.py
@@ -1,16 +1,30 @@
 from app.database import Base
-from sqlalchemy import String, ForeignKey, Boolean
+from sqlalchemy import JSON, DateTime, String, ForeignKey, Boolean, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from pydantic import AnyUrl
+from datetime import datetime,timezone
+
+def utc_now():
+    return datetime.now(timezone.utc)
 
 class Article(Base):
     __tablename__ = 'articles'
     
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True,index=True)
     feed_id: Mapped[int] = mapped_column(ForeignKey("feeds.id"))
-    title: Mapped[str] = mapped_column(String(20))
-    summary: str (описание, может быть пустым)
-    link: str (уникальная ссылка на оригинал)
-    published_at: datetime
-    is_read: bool = False
-    tags: JSON (список строк, например: ["python", "backend"])
-    created_at: datetime (когда добавлено в БД)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    summary: Mapped[str] = mapped_column(String(2000), nullable=True)
+    link: Mapped[str] = mapped_column(String(255), nullable=False,unique=True)
+    published_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    is_read: Mapped[bool] = mapped_column(Boolean,default=False)
+    tags: Mapped[JSON] = mapped_column(JSON, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=utc_now)
+    
+    feeds: Mapped["Feed"] = relationship("Feed",back_populates='articles')
+    
+    def __repr__(self):
+        return f"<User(id={self.id}, username='{self.title}')>"
+    
+    __table_args__ = (
+        UniqueConstraint('feed_id', 'link', name='uq_feed_link'),
+    )
--- app/Feed/models.py
+++ app/Feed/models.py
@@ -1,3 +1,4 @@
+from typing import List
 from app.database import Base
 from sqlalchemy import String, ForeignKey, Boolean
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -11,7 +12,8 @@ class Feed(Base):
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
     
+    articles: Mapped[List["Article"]] = relationship("Article", back_populates="feed")
     user: Mapped["User"] = relationship("User", back_populates="feeds")
     
     def __repr__(self):
-        return f"<User(id={self.id}, username='{self.username}')>"
+        return f"<User(id={self.id}, username='{self.title}')>"
